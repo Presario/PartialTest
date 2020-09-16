061a3c9ed4d9e7ffb666d9e7faf2cd8758ce2f51
msi <322890@qq.com>
2020-09-16 22:43:06 +0800

    添加项目文件。

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace PartialTest.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public string tag = "";
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPostAsync()
+        {
+            
+            _logger.LogInformation("debug!");
+            _logger.LogInformation(TempData["holi"].ToString());
+            _logger.LogInformation(Request.Form["temp"]);
+            return Page();
+        }
+
+    }
+}
